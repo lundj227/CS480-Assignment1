26e4f2e43b3da64e45a36c31517915dfb01b6107
Jack Lund <jlund3@uoregon.edu>
2026-04-07 16:36:05 -0700

    added pickups

--- Assignment-1/Assets/Scripts/PlayerController.cs
+++ Assignment-1/Assets/Scripts/PlayerController.cs
@@ -3,28 +3,53 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    public float speed = 0;
+ // Rigidbody of the player.
+ private Rigidbody rb; 
 
-    
-    private Rigidbody rb;
-    private float movementX;
-    private float movementY;
-    void Start()
+ // Movement along X and Y axes.
+ private float movementX;
+ private float movementY;
+
+ // Speed at which the player moves.
+ public float speed = 0; 
+
+ // Start is called before the first frame update.
+ void Start()
     {
-        rb = GetComponent <Rigidbody>();
+ // Get and store the Rigidbody component attached to the player.
+        rb = GetComponent<Rigidbody>();
     }
-
-    void OnMove(InputValue movementValue)
-    { 
+ 
+ // This function is called when a move input is detected.
+ void OnMove(InputValue movementValue)
+    {
+ // Convert the input value into a Vector2 for movement.
         Vector2 movementVector = movementValue.Get<Vector2>();
+
+ // Store the X and Y components of the movement.
         movementX = movementVector.x; 
         movementY = movementVector.y; 
     }
 
-    void FixedUpdate()
+ // FixedUpdate is called once per fixed frame-rate frame.
+ void FixedUpdate() 
     {
+ // Create a 3D movement vector using the X and Y inputs.
         Vector3 movement = new Vector3 (movementX, 0.0f, movementY);
-        rb.AddForce(movement * speed);
+
+ // Apply force to the Rigidbody to move the player.
+        rb.AddForce(movement * speed); 
     }
+
+ void OnTriggerEnter(Collider other) 
+    {
+ // Check if the object the player collided with has the "PickUp" tag.
+ if (other.gameObject.CompareTag("PickUp")) 
+        {
+ // Deactivate the collided object (making it disappear).
+            other.gameObject.SetActive(false);
+        }
+    }
+
+
 }
