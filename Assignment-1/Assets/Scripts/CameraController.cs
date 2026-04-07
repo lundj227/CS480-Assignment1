b8aa4a115d738d8e9164bae88fd0eba312ec8102
Jack Lund <jlund3@uoregon.edu>
2026-04-07 15:42:02 -0700

    added character and camera movement

---
+++ Assignment-1/Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public GameObject player;
+    private Vector3 offset;
+
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset; 
+    }
+}
